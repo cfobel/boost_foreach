397b259d84c17e7b01141df276b20d07342be2de
Christian Fobel <christian@fobel.net>
2010-09-30 14:26:21 -0400

    Initial commit of Boost.Foreach example

---
+++ foreach.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <boost/foreach.hpp>
+
+int main()
+{
+    std::string hello( "Hello, world!" );
+    
+    BOOST_FOREACH( char ch, hello )
+    {
+        std::cout << ch;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
